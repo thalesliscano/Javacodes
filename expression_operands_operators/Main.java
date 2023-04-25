ddcfae35c3f3770672a7cc106af7e3c4fe898515
Thales Liscano Carvalho <202119070668@DOMINIO.FACOM.UFMS.BR>
2023-04-24 21:21:41 -0400

    first commit

---
+++ expression_operands_operators/Main.java
@@ -0,0 +1,12 @@
+package expression_operands_operators;
+public class Main {
+    public static void main(String[] args) {
+        int friends = 10;
+
+        friends = friends + 1;
+        System.out.println(friends);
+
+        
+    }
+    
+}
